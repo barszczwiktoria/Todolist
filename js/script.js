37084e31263175553aa7d226d606014592f03812
barszczwiktoria <barszcz.wiktoria@gmail.com>
2023-03-03 16:35:22 +0100

    adding new task and showing if task is done

--- js/script.js
+++ js/script.js
@@ -10,22 +10,47 @@
         },
     ];
 
-const render = () => {
-let htmlString = "";
+    const render = () => {
+        let htmlString = "";
 
-for (const task of tasks) {
-    htmlString += `
-    <li>
+        for (const task of tasks) {
+            htmlString += `
+    <li
+    ${task.done ? " style=\"text-decoration: line-through\"" : ""} 
+    >
     ${task.content}
     </li> 
     `;
-}
-document.querySelector('.js-tasks').innerHTML =htmlString;
-};
-
-    const init = () => {
-render();
+        }
+        document.querySelector('.js-tasks').innerHTML = htmlString;
     };
 
-init ();
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit)
+    };
+
+    init();
 }
