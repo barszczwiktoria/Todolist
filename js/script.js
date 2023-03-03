44547231ac44b183073fa8b2e77cec9fc92927d2
barszczwiktoria <barszcz.wiktoria@gmail.com>
2023-03-03 11:46:53 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+{
+    const tasks = [
+        {
+            content: "nagrać lekcję",
+            done: false,
+        },
+        {
+            content: "zjeść piergoi",
+            done: true,
+        },
+    ];
+
+const render = () => {
+let htmlString = "";
+
+for (const task of tasks) {
+    htmlString += `
+    <li>
+    ${task.content}
+    </li> 
+    `;
+}
+document.querySelector('.js-tasks').innerHTML =htmlString;
+};
+
+    const init = () => {
+render();
+    };
+
+init ();
+}
